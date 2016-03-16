c2ec9485bc12e030239c3bc508867e3be7e73cce
k3roulas <k3roulas@gmail.com>
2016-03-16 01:20:40 +0000

    Service it

---
+++ www/js/services.js
@@ -0,0 +1,201 @@
+/**
+ * A key value storage
+ */
+app.service('store', function() {
+
+    var storage = window.localStorage;
+
+    this.load = function(key) {
+
+        var storedNews =  storage.getItem(key);;
+        if (storedNews !== null) {
+            return JSON.parse(storedNews);
+        }
+        return null;
+    };
+
+    this.save = function(key, value) {
+        storage.setItem(key, JSON.stringify(value));
+    };
+});
+
+
+app.service('pullToRefreshService', ['$timeout', '$ionicScrollDelegate', function($timeout, $ionicScrollDelegate) {
+
+    /**
+     * Trigger the pull-to-refresh on a specific scroll view delegate handle.
+     * @param {string} delegateHandle - The `delegate-handle` assigned to the `ion-content` in the view.
+     */
+    this.triggerPtr = function(delegateHandle) {
+
+        $timeout(function() {
+
+            var scrollView = $ionicScrollDelegate.$getByHandle(delegateHandle).getScrollView();
+
+            if (!scrollView) return;
+
+            scrollView.__publish(
+                scrollView.__scrollLeft, -scrollView.__refreshHeight,
+                scrollView.__zoomLevel, true);
+
+            var d = new Date();
+
+            scrollView.refreshStartTime = d.getTime();
+
+            scrollView.__refreshActive = true;
+            scrollView.__refreshHidden = false;
+            if (scrollView.__refreshShow) {
+                scrollView.__refreshShow();
+            }
+            if (scrollView.__refreshActivate) {
+                scrollView.__refreshActivate();
+            }
+            if (scrollView.__refreshStart) {
+                scrollView.__refreshStart();
+            }
+
+        });
+
+    }
+}]);
+
+app.service('appConfig', ['store', function(store) {
+
+    var prefix = '';
+    if (false === develop) {
+        prefix = 'http://www.bbc.co.uk';
+    }
+
+    var defaultConfig =  {
+        podcasts : [
+            {
+                source : 'BBC Radio 4',
+                rssFlows : [
+                    { url : prefix + '/programmes/b006qtl3/episodes/downloads.rss', max: 7, label: "The world tonight", dayWeight: 124, enabled: true },
+                    { url : prefix + '/programmes/b006qjxt/episodes/downloads.rss', max: 7, label: "The six o'clock", dayWeight: 118, enabled: true },
+                    { url : prefix + '/programmes/b006qptc/episodes/downloads.rss', max: 7, label: "World at one", dayWeight: 113, enabled: true },
+                ]
+            },
+            {
+                source : 'Global News Podcast',
+                rssFlows : [
+                    { url : prefix + '/programmes/p02nq0gn/episodes/downloads.rss', max: 14, label: "Global News Podcast", dayWeight: 105, enabled: true },
+                ]
+            }
+        ]
+    };
+
+    this.config = {};
+    this.load = function() {
+        var config = store.load('config');
+        if (config === null) {
+            store.save('config', defaultConfig);
+        }
+        this.config = config;
+    }
+
+}]);
+
+app.service('newsProvider', ['$http', 'x2js', '$q', 'store', 'appConfig', '$rootScope', function($http, x2js, $q, store, appConfig, $rootScope) {
+
+    this.newsContainer = {
+        news: []
+    };
+
+    this.refresh = function(auto) {
+
+        var newsList = [];
+        var promises = [];
+
+        // Create a closure to embed the rss and source in the response
+        var success = function (rss, source) {
+
+            var localRss = rss;
+            var localSource = source;
+
+            return function (result, source) {
+
+                var rssObj = x2js.xml_str2json(result.data);
+                var items = rssObj.rss.channel.item;
+
+                for (var itemPos = 0; itemPos < items.length; itemPos++) {
+
+                    // keep only the first
+                    if (itemPos == localRss.max) {
+                        break;
+                    }
+
+                    var item = items[itemPos];
+                    var link = item.link;
+                    var theDate = new Date();
+                    theDate.setTime(Date.parse(item.pubDate));
+                    newsList.push(
+                        {
+                            dayWeight: localRss.dayWeight,
+                            theDate: theDate,
+                            url: link,
+                            label: item.title,
+                            source: localSource,
+                            class: localSource.replace(/ /g, ""),
+                            flow: localRss.label,
+                            progress: 0
+                        }
+                    )
+
+                }
+            }
+        };
+
+
+        var podcasts = appConfig.config.podcasts;
+        for (var podCastEnum = 0; podCastEnum < podcasts.length; podCastEnum++) {
+
+            var podcast = podcasts[podCastEnum];
+
+            for (var rssFlowEnum = 0; rssFlowEnum < podcast.rssFlows.length; rssFlowEnum++) {
+
+                var theRss = podcast.rssFlows[rssFlowEnum];
+
+                if (theRss.enabled) {
+                    promises.push(
+                        $http.get(theRss.url).then(
+                            // Success
+                            success(theRss, podcast.source),
+                            // Error
+                            function (error) {
+                                alert(JSON.stringify(error));
+                                // todo
+                            }
+                        )
+                    );
+                }
+            }
+
+        }
+
+        var newsContainer = this.newsContainer;
+        $q.all(promises).then(function () {
+
+            $rootScope.$broadcast('scroll.refreshComplete');
+
+            // Sort list by date desc
+            var newList = newsList.sort(function (a, b) {
+                var aDate = new Date(a.theDate).getTime();
+                var bDate = new Date(b.theDate).getTime();
+                if (aDate == bDate) {
+                    return b.dayWeight - a.dayWeight;
+                }
+                return bDate - aDate;
+            });
+
+            newsContainer.news = newList;
+            console.log(newList);
+            store.save('news', newList);
+
+        });
+    }
+
+
+}]);
+
+
